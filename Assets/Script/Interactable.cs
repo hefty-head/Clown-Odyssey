692391d00938f9cc115ba1bd842c6842e44ecc31
aaboaf <aaboaf@uchicago.edu>
2018-02-20 21:32:07 -0600

    Text is working
    yeet

--- Assets/Script/Interactable.cs
+++ Assets/Script/Interactable.cs
@@ -1,25 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interactable : MonoBehaviour {
 
 	public bool talks;
+	private int numMessages = 2;
 	public string message;
 	public GameObject player;//= GameObject.FindWithTag ("Player");
-
+	public GameObject canvas;
+	public Text txt;
 	public virtual void OnInteract()
 	{
 		
 		Player playerScript = player.GetComponent<Player>();
-		if (playerScript.canMove) 
+		if (playerScript.canMove && numMessages == 2)
 		{
 			playerScript.canMove = false;
-			Debug.Log ("eat pa");
-		} 
-		else if (!playerScript.canMove) 
+			txt.text = "Eat pant asshole";
+			canvas.SetActive (true);
+			numMessages--;
+
+
+		}
+		else if (numMessages == 1)
 		{
+			txt.text = "Q to the bourgeoisie";
+			numMessages--;
+		}
+		else if (!playerScript.canMove && numMessages == 0) 
+		{
+			canvas.SetActive (false);
 			playerScript.canMove = true;
+			numMessages = 2;
 		}
 		//if(Input.GetButtonDown("Cancel"))
 		//playerScript.canMove = true;
