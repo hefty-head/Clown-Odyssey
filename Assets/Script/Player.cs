074dca565409a6131c488370fc5be74719c36eb9
Alvf <33241910+Alvf@users.noreply.github.com>
2018-02-26 21:46:59 -0600

    Add files via upload

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	public bool canMove = true;
+	private Vector3 moveDelta;
+	private BoxCollider2D boxCollider;
+	private RaycastHit2D hit;
+    private Animator animator;
+	private void Start () {
+		boxCollider = GetComponent<BoxCollider2D> ();
+		DontDestroyOnLoad (this);
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		// reset move delta
+		if (!canMove)
+			return;
+        animator.SetInteger("State", 0);
+        float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+		moveDelta = new Vector3 (x, y, 0);
+
+        //swap direction for right/left
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetInteger("State", 1);
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetInteger("State", 1);
+        }
+        hit = Physics2D.BoxCast (transform.position, boxCollider.size, 0, new Vector2 (0, moveDelta.y), Mathf.Abs (moveDelta.y * Time.deltaTime), LayerMask.GetMask ("Actor", "Blocking"));
+		if(hit.collider == null || hit.collider.tag == "Trigger")
+			transform.Translate (0,moveDelta.y * Time.deltaTime, 0);
+		hit = Physics2D.BoxCast (transform.position, boxCollider.size, 0, new Vector2 ( moveDelta.x,0), Mathf.Abs (moveDelta.x * Time.deltaTime), LayerMask.GetMask ("Actor", "Blocking"));
+		if(hit.collider == null || hit.collider.tag == "Trigger")
+			transform.Translate (moveDelta.x * Time.deltaTime, 0,0);
+
+    }
+	public void mutateCanMove()
+	{
+		canMove = !canMove;
+	}
+}
